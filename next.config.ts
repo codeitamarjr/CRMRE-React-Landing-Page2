8ad7c970f38683a2e567ecb990aa93865fd9a60d
Itamar Junior <hello@itjunior.dev>
2025-02-26 23:48:53 +0000

    update Next.js configuration for production settings and add homepage in package.json

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  output: 'export',
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  assetPrefix: isProd ? '/CRMRE-Landing-Page2/' : '',
+  basePath: isProd ? '/CRMRE-Landing-Page2' : '',
+  output: 'export',
 };
 
 export default nextConfig;
