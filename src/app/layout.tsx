641e80e1cace0f73be2f19b5a3ef8be3fc79671e
Itamar Junior <hello@itjunior.dev>
2025-04-25 12:05:27 +0100

    Add Matomo analytics tracking script to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,32 @@ export default function RootLayout({
     `,
           }}
         />
+
+        {/* Matomo */}
+        <Script
+          id="matomo-tracker"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u = "https://analytics.itjunior.dev/";
+                _paq.push(['setTrackerUrl', u + 'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d = document,
+                  g = d.createElement('script'),
+                  s = d.getElementsByTagName('script')[0];
+                g.async = true;
+                g.src = u + 'matomo.js';
+                s.parentNode.insertBefore(g, s);
+              })();
+            `,
+          }}
+        />
+
       </head>
 
       <body
